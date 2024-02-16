dc3ea90dcc63d59400810d1be469e74a5ec7461e
ch1llc0d3 <chillcrib7@gmail.com>
2024-02-16 19:01:07 -0300

    main.tf

--- Test1/Test1/main.tf
+++ Test1/Test1/main.tf
@@ -3,21 +3,21 @@ module "aws_security_group" {
   source = "./modules/sec_group"
 }
 
-module "sec_group" {
-  source              = "./modules/sec_group"
+# module "sec_group" {
+#   source              = "./modules/sec_group"
 
-  # No additional inputs are needed
-}
+# }
 
-module "ec2" {
-  source              = "./modules/ec2"
-  aws_security_group  = module.security_groups.aws_security_group  // Pass the AWS security group module reference
-  // Ensure that security_groups variable is provided if needed by the ec2 module
-  # security_groups     = module.security_groups.security_group_id
-  security_groups = [ module.security_groups.sec_group.id ]
-}
+# module "ec2" {
+#   source              = "./modules/ec2"
+#   aws_security_group  = module.security_groups.aws_security_group  // Pass the AWS security group module reference
+#   // Ensure that security_groups variable is provided if needed by the ec2 module
+#   # security_groups     = module.security_groups.security_group_id
+#   security_groups = [ module.security_groups.sec_group.id ]
+# }
 
-# Output variables if needed
-output "ec2_instance_id" {
-  value = module.aws_security_group
-}
+# # Output variables if needed
+# output "ec2_instance_id" {
+#   value = module.aws_security_group
+# }
+
