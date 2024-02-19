aca5e2014b580245222b96c67a821d9d4b075773
ch1llc0d3 <chillcrib7@gmail.com>
2024-02-19 17:36:47 -0300

    variables

--- Test1/Test1/variables.tf
+++ Test1/Test1/variables.tf
@@ -1,43 +1,21 @@
-variable "ami_id" {
-    description = "ami id"
-    type    = string
+variable "ami" {
+    type = string
     default = "ami-12345667"
 }
 
 variable "instance_type" {
-    description = "instance_type"
-    type    = string
+    type = string
     default = "t3.micro"
 }
-
-variable "iam_instance_profile" {
-    description = "iam instance profile"
-    type    = string
-    default = "rn:aws:iam::123456789012:instance-profile/ecsInstanceRole"
-}
-
-variable "Hello" {
-    description = "just saying hello"
+variable "hola" {
     type = string
     default = "HelloWorld"
 }
 
-#   security_groups = [ aws_security_group.sec_group.id ]
-
-variable "security_groups" {
-    description = "id output from sec_group"
+variable "iam_instance_profile" {
     type = string
+    default = "arn:aws:iam::123456789012:instance-profile/ecsInstanceRole"
 }
 
-# output "security_groups" {
-#     value = aws_security_group.sg.id
-# }
+security_groups = [ aws_security_group.sec_group.id ]
 
-variable "aws_security_group" {
-    description = "output from sec_group"
-    type = string
-}
-
-# output "aws_security_group" {
-#     value = aws_security_group.sg
-# }
