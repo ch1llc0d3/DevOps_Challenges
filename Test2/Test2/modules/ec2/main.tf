92e126d05b1e0ca97faf4a46a845883faf316722
ch1llc0d3 <chillcrib7@gmail.com>
2024-02-19 17:39:21 -0300

    stuff

--- Test2/Test2/modules/ec2/main.tf
+++ Test2/Test2/modules/ec2/main.tf
@@ -3,7 +3,7 @@ resource "aws_instance" "web" {
   instance_type = var.instance_type
   # security_groups = [ aws_security_group.sec_group.id ]
   # security_groups      = [module.security_group.security_group_id]
-    security_groups = [var.security_group_id]
+  security_groups = [var.security_group_id]
   # iam_instance_profile = aws_iam_instance_profile.instance_profle.arn
   # iam_instance_profile = var.aws_iam_instance_profile.instance_profle.arn
   iam_instance_profile = var.iam_instance_profile_arn
