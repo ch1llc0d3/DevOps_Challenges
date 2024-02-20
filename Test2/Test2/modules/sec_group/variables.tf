0370a0f25e7417fe85310d93d44010cb8159eefb
ch1llc0d3 <chillcrib7@gmail.com>
2024-02-20 18:44:36 -0300

    changes

---
+++ Test2/Test2/modules/sec_group/variables.tf
@@ -0,0 +1,41 @@
+variable "vpc_id" {
+  default = "vpc-12345678"
+}
+
+variable "ingress_rules" {
+  description = "List of ingress rules"
+  type = list(object({
+    from_port   = number
+    to_port     = number
+    protocol    = string
+    cidr_blocks = list(string)
+  }))
+  default = [
+    {
+      from_port   = 22
+      to_port     = 22
+      protocol    = "tcp"
+      cidr_blocks = ["192.168.1.0/24", "192.168.2.0/24"]
+    }
+    # Add more ingress rules as needed
+  ]
+}
+
+variable "egress_rules" {
+  description = "List of egress rules"
+  type = list(object({
+    from_port   = number
+    to_port     = number
+    protocol    = string
+    cidr_blocks = list(string)
+  }))
+  default = [
+    {
+      from_port   = 0
+      to_port     = 0
+      protocol    = "-1"
+      cidr_blocks = ["0.0.0.0/0"]
+    }
+    # Add more egress rules as needed
+  ]
+}
