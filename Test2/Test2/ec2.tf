f3994ae79edd89d7de4ffb82d789a766d36c1cd6
ch1llc0d3 <chillcrib7@gmail.com>
2024-02-20 18:22:32 -0300

    ec2

--- Test2/Test2/ec2.tf
+++ Test2/Test2/ec2.tf
@@ -1,10 +1,10 @@
 resource "aws_instance" "web" {
-  ami           = "ami-12345667"
-  instance_type = "t3.micro"
-  security_groups = [ aws_security_group.sec_group.id ]
-  iam_instance_profile = aws_iam_instance_profile.instance_profle.arn
+  ami           = var.ami
+  instance_type = var.instance_type
+  # security_groups = [ aws_security_group.sec_group.id ]
+  # iam_instance_profile = aws_iam_instance_profile.instance_profle.arn
 
   tags = {
-    Name = "HelloWorld"
+    Name = var.hola
   }
 }
