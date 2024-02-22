c7694cada20d7de07c5eb08bced51610f56f7113
ch1llc0d3 <128004915+ch1llc0d3@users.noreply.github.com>
2024-02-22 16:07:12 -0300

    Update main.tf

--- Test3/main.tf
+++ Test3/main.tf
@@ -1,10 +1,11 @@
-
+# Module for creating the VPC
 module "vpc" {
   source         = "./modules/vpc"
   vpc_cidr_block = "10.0.0.0/16"
   vpc_name       = "my_vpc"
 }
 
+# Module for creating subnets within the VPC
 module "subs" {
   source               = "./modules/subs"
   vpc_id               = module.vpc.vpc_id
@@ -12,12 +13,41 @@ module "subs" {
   private_subnet_cidrs = ["10.0.3.0/24", "10.0.4.0/24"]
 }
 
+# Module for creating routing tables and associations
 module "route_s" {
   source            = "./modules/route_s"
   vpc_id            = module.vpc.vpc_id
   public_subnet_ids = module.subs.public_subnet_ids
 }
 
+# Module for creating EC2 instances
 module "ec2" {
   source = "./modules/ec2"
 }
+
+# Module for creating security groups
+module "security" {
+  source                       = "./modules/security"
+  alb_security_group_id        = module.security.alb_security_group_id  # Provide the ALB security group ID here
+  instance_security_group_name = "example-instance-sg"
+  vpc_id                       = module.vpc.vpc_id
+}
+
+
+# Module for creating the Application Load Balancer (ALB)
+module "alb" {
+  source            = "./modules/alb"
+  alb_name          = "my-alb"
+  internal          = false
+  subnets           = module.subs.public_subnet_ids
+  security_group_id = module.security.alb_security_group_id
+  target_group_name = "my-target-group"
+  vpc_id            = module.vpc.vpc_id
+}
+
+output "alb_dns_name" {
+  description = "DNS name of the Application Load Balancer (ALB)"
+  value       = module.alb.alb_dns_name
+}
+
+
