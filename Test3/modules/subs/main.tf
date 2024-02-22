ea75b2fe76faa9c2660988294fbb9569ca28bc41
ch1llc0d3 <chillcrib7@gmail.com>
2024-02-22 10:45:25 -0300

    modularizing the 1st part

--- Test3/modules/subs/main.tf
+++ Test3/modules/subs/main.tf
@@ -1,12 +1,12 @@
 resource "aws_subnet" "public_subnets" {
-  count     = length(var.public_subnet_cidrs)
-  vpc_id    = var.vpc_id
+  count      = length(var.public_subnet_cidrs)
+  vpc_id     = var.vpc_id
   cidr_block = var.public_subnet_cidrs[count.index]
 }
 
 resource "aws_subnet" "private_subnets" {
-  count     = length(var.private_subnet_cidrs)
-  vpc_id    = var.vpc_id
+  count      = length(var.private_subnet_cidrs)
+  vpc_id     = var.vpc_id
   cidr_block = var.private_subnet_cidrs[count.index]
 }
 
