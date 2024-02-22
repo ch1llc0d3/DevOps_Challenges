af5289477cd911d3184e7485ced104c8a0169ab5
ch1llc0d3 <128004915+ch1llc0d3@users.noreply.github.com>
2024-02-22 16:11:55 -0300

    Update var.tf

--- Test3/modules/subs/var.tf
+++ Test3/modules/subs/var.tf
@@ -1,5 +1,6 @@
 variable "vpc_id" {
   description = "ID of the VPC."
+  type        = string
 }
 
 variable "public_subnet_cidrs" {
@@ -11,9 +12,3 @@ variable "private_subnet_cidrs" {
   description = "CIDR blocks for private subnets."
   type        = list(string)
 }
-
-# variable "private_subnets" {
-#   description = "CIDR blocks for private subnets"
-#   type        = list(string)
-#   default     = ["10.0.3.0/24", "10.0.4.0/24"]  # Default CIDR blocks for private subnets
-# }
