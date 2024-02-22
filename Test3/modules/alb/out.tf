ebb10b1a4f049cb0959b8158618762de89d603e3
ch1llc0d3 <chillcrib7@gmail.com>
2024-02-22 17:57:47 -0300

    fresh start

--- Test3/modules/alb/out.tf
+++
@@ -1,9 +0,0 @@
-output "alb_dns_name" {
-  description = "DNS name of the Application Load Balancer (ALB)"
-  value       = aws_lb.my_alb.dns_name
-}
-
-output "alb_zone_id" {
-  description = "Zone ID of the Application Load Balancer (ALB)"
-  value       = aws_lb.my_alb.zone_id
-}
--- Test3/modules/route_s/var.tf
+++
@@ -1,8 +0,0 @@
-variable "vpc_id" {
-  description = "ID of the VPC."
-}
-
-variable "public_subnet_ids" {
-  description = "IDs of the public subnets."
-  type        = list(string)
-}
